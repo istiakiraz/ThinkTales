005918aae4a46044da4571cf2df4d673bb632b60
istiakiraz <istiakiraz@gmail.com>
2025-06-12 03:02:29 +0600

    added a addblog form input

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -1,31 +1,29 @@
-import React, { use } from 'react';
-import { AuthContext } from '../provider/AuthProvider';
-import { Link } from 'react-router';
-import { GoArrowLeft } from 'react-icons/go';
-import logo from '../assets/titleLogo.png'
-import write from '../assets/coding-3-24.svg'
+import React, { use } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { Link } from "react-router";
+import { GoArrowLeft } from "react-icons/go";
+import logo from "../assets/titleLogo.png";
+import write from "../assets/coding-3-24.svg";
 import { motion } from "framer-motion";
-
+import { MdDescription, MdOutlineSubtitles } from "react-icons/md";
+import { TbCategory2, TbFileDescription, TbPhotoPlus } from "react-icons/tb";
 
 const AddBlog = () => {
+  // const Toast = Swal.mixin({
+  //     toast: true,
+  //     position: "top-end",
+  //     showConfirmButton: false,
+  //     timer: 3000,
+  //     timerProgressBar: true,
+  //     didOpen: (toast) => {
+  //       toast.onmouseenter = Swal.stopTimer;
+  //       toast.onmouseleave = Swal.resumeTimer;
+  //     },
+  //   });
 
-    // const Toast = Swal.mixin({
-    //     toast: true,
-    //     position: "top-end",
-    //     showConfirmButton: false,
-    //     timer: 3000,
-    //     timerProgressBar: true,
-    //     didOpen: (toast) => {
-    //       toast.onmouseenter = Swal.stopTimer;
-    //       toast.onmouseleave = Swal.resumeTimer;
-    //     },
-    //   });
+  // const navigate = useNavigate()
 
-
-// const navigate = useNavigate()
-
-//   const [dob, setDob] = useState(null);
-  const {user, isDark} = use(AuthContext)
+  const { user } = use(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -35,13 +33,15 @@ const AddBlog = () => {
     const formData = new FormData(form);
     const newTask = Object.fromEntries(formData.entries());
 
-    console.log(newTask);
+    const today = new Date();
+    const dateOnly = today.toISOString().split("T")[0];
 
-    // newTask.budget = parseInt(newTask.budget);
-    // newTask.dob = dob ? format(dob, "yyyy-MM-dd") : "";
-    // newTask.bid = parseInt(0)
-    newTask.userPhoto = user?.photoURL
-    // console.log(newTask);
+    newTask.userPhoto = user?.photoURL;
+    newTask.userName = user?.displayName;
+    newTask.userEmail = user?.email;
+    newTask.date = dateOnly;
+
+    console.log(newTask);
 
     // send task data to the DB
     // fetch('https://kajero-server.vercel.app/tasks',{
@@ -58,152 +58,139 @@ const AddBlog = () => {
     //         title: "Your add new task successfully!",
     //       });
     // })
-
-
   };
 
   return (
-    <div
-   className={` pt-18 -mt-18 ${isDark? 'bg-[#D2D0A0]/20 *:text-black  ' : '' }`}
-    >
-      <div className="lg:w-9/12 w-11/12 py-16 mx-auto">
-        <Link to="/">
-                <button className="btn mb-5 col-span-full relative rounded px-5 py-2.5 overflow-hidden group bg-[#49785b]  hover:bg-gradient-to-r hover:from-[#49785b] hover:to-[#49785b] text-white hover:ring-2 hover:ring-offset-2 hover:ring-[#49785b] transition-all ease-out duration-300  ">
-              <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-              <span className="relative flex gap-1 items-center"> <GoArrowLeft size={20} />Back to Home</span>
-            </button>
-              </Link>
+    <div className="lg:w-9/12 w-11/12 py-16 mx-auto">
+      <Link to="/">
+        <button className="btn mb-5 col-span-full relative rounded px-5 py-2.5 overflow-hidden group bg-[#4c637c]  hover:bg-gradient-to-r hover:from-[#4c637c] hover:to-[#4c637c] text-white hover:ring-2 hover:ring-offset-2 hover:ring-[#4c637c] transition-all ease-out duration-300  ">
+          <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          <span className="relative flex gap-1 items-center">
+            {" "}
+            <GoArrowLeft size={20} />
+            Back to Home
+          </span>
+        </button>
+      </Link>
 
-        <div className="bg-[#d6edeb]/50 rounded-xl">
-          <div className="lg:w-8/12  lg:px-12 w-11/12 py-12 mx-auto">
-            <h1
-              className="text-center shadow-[0px_0px_20px_0px_rgba(76,99,124,0.8)] text-gray-600 lg:px-12 text-3xl mb-8 w-fit mx-auto flex flex-col items-center  bg-[#4c637c]/20 p-4 rounded-2xl "
-            > <motion.img 
-             initial={{ opacity: 0, y: -30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.9 }}
-            className='w-56 -mb-8' src={write} alt="" />
-               Write Your Unwritten Thoughts on <br /> <img className='w-64' src={logo} alt="ThinkTales" />
-            </h1>
-            <p className="hyphens-auto text-justify font-medium mb-8">
-             Every idea starts as a whisper in your mind — a fleeting thought, a quiet observation, a moment of emotion. Here at ThinkTales™, we invite you to give those thoughts a voice. This is your space to reflect, express, and explore.
-            </p>
+      <div className="bg-[#4c637c]/20 rounded-xl">
+        <div className="lg:w-8/12  lg:px-12 w-11/12 py-12 mx-auto">
+          <h1 className="text-center shadow-[0px_0px_20px_0px_rgba(76,99,124,0.8)] text-gray-600 lg:px-12 text-3xl mb-8 w-fit mx-auto flex flex-col items-center  bg-[#4c637c]/20 p-4 rounded-2xl ">
+            {" "}
+            <motion.img
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.9 }}
+              className="w-56 -mb-8"
+              src={write}
+              alt=""
+            />
+            Write Your Unwritten Thoughts on <br />{" "}
+            <img className="w-64" src={logo} alt="ThinkTales" />
+          </h1>
+          <p className="hyphens-auto text-justify font-medium mb-8">
+            Every idea starts as a whisper in your mind — a fleeting thought, a
+            quiet observation, a moment of emotion. Here at ThinkTales™, we
+            invite you to give those thoughts a voice. This is your space to
+            reflect, express, and explore.
+          </p>
 
-            <form
-              onSubmit={handleSubmit}
-              className="grid grid-cols-1 items-start justify-center lg:gap-10 lg:grid-cols-2"
+          <form onSubmit={handleSubmit} className="">
+            <div>
+              <fieldset className="fieldset rounded-box   p-4">
+                {/* title input */}
+                <label className="label font-bold">
+                  {" "}
+                  <MdOutlineSubtitles size={20} /> Blog Title
+                </label>
+                <input
+                  required
+                  name="title"
+                  type="text"
+                  className="input bg-gray-200 placeholder:text-gray-600  mb-2 w-full"
+                  placeholder="Enter your blog title..."
+                />
+
+                {/* dopdown category */}
+                <label className="label font-bold">
+                  {" "}
+                  <TbCategory2 size={20} />
+                  Category
+                </label>
+                <select
+                  name="category"
+                  required
+                  defaultValue=""
+                  className="input mb-2 bg-gray-300 placeholder:text-gray-600  lg:w-52 w-full"
+                >
+                  <option disabled value="">
+                    Pick a blog category
+                  </option>
+                  <option>Lifestyle</option>
+                  <option>Technology</option>
+                  <option>Health & Wellness</option>
+                  <option>Film & TV</option>
+                  <option>Art & Design</option>
+                  <option>Music & Culture</option>
+                  <option>Photography</option>
+                  <option>Finance & Investing</option>
+                  <option>Fashion & Style</option>
+                  <option>Education</option>
+                  <option>Food & Recipes</option>
+                  <option>Travel</option>
+                  <option>Gaming & Esports</option>
+                  <option>Books & Literature</option>
+                  <option>Parenting & Family</option>
+                  <option>Personal Growth</option>
+                </select>
+
+                {/* blog photo url input */}
+                <label className=" font-bold label">
+                  {" "}
+                  <TbPhotoPlus size={20} /> Featured Photo URL
+                </label>
+                <input
+                  required
+                  type="text"
+                  name="photo"
+                  className="input bg-gray-200 placeholder:text-gray-600 mb-2 w-full lg:w-52"
+                  placeholder="Add a featured image URL..."
+                />
+
+                {/*Short Description input */}
+                <label className=" font-bold   label">
+                  {" "}
+                  <TbFileDescription size={20} /> Short Description
+                </label>
+                <textarea
+                  required
+                  name="short_Description"
+                  className="textarea textarea-bordered bg-gray-200 placeholder:text-gray-600 mb-2 h-18 text-start  w-full"
+                  placeholder="Describe your blog in a few words... "
+                />
+
+                {/*Long Description input */}
+                <label className=" font-bold   label">
+                  {" "}
+                  <MdDescription size={20} /> Long Description
+                </label>
+                <textarea
+                  required
+                  name="long_Description"
+                  className="textarea textarea-bordered bg-gray-200 placeholder:text-gray-600 mb-2 h-28 text-start  w-full"
+                  placeholder="Write your complete blog post here... "
+                />
+              </fieldset>
+            </div>
+
+            <button
+              type="submit"
+              className="btn col-span-full flex w-full lg:w-[845px] relative rounded px-5 py-2.5 overflow-hidden group bg-[#4c637c]  hover:bg-gradient-to-r hover:from-[#4c637c] hover:to-[#4c637c] text-white hover:ring-2 hover:ring-offset-2 hover:ring-[#4c637c] transition-all ease-out duration-300  "
             >
-              <div>
-                <fieldset className="fieldset rounded-box   p-4">
-                  {/* title input */}
-                  <label className="label font-bold">Task Title</label>
-                  <input
-                    required
-                    name="title"
-                    type="text"
-                    className="input  mb-2 lg:w-96 w-full"
-                    placeholder="Enter task title"
-                  />
-
-                  {/* dopdown category */}
-                  <label className="label font-bold">Category</label>
-                  <select
-                    name="category"
-                    required
-                
-                    className="input mb-2 lg:w-96 w-full"
-                  >
-                    <option disabled={true} value="">Select a category</option>
-                    <option value="Web Development"> Web Development</option>
-                    <option value=" Design">Design</option>
-                    <option value="Writing">Writing</option>
-                    <option value="Marketing">Marketing</option>
-                    <option value="SEO">SEO</option>
-                    <option value="Video Edit">Video Edit</option>
-                    <option value="Photo Edit">Photo Edit</option>
-                    <option value="Others">Others</option>
-                  </select>
-
-                  {/* day pick  */}
-                  {/* <label className=" font-bold label">Date of Deadline</label>
-                  <DatePicker
-                    required
-                    selected={dob}
-                    onChange={(date) => setDob(date)}
-                    dateFormat="dd/MM/yyyy"
-                    placeholderText="Select your task deadline"
-                    className="border border-gray-300 placeholder:text-sm bg-white lg:w-96 w-full px-3 py-2 rounded"
-                  /> */}
-
-                   {/* Description input */}
-                  <label className=" font-bold  label">Description</label>
-                  <textarea
-                    required
-                    name="description"
-                    className="textarea textarea-bordered mb-2 h-28 text-start lg:w-96 w-full"
-                    placeholder="Enter task description"
-                  />
-
-                
-                </fieldset>
-              </div>
-
-              <div>
-                <fieldset className="fieldset -space-y-[2px] -mt-5 md:-mt-0 rounded-box   p-4">
-                  
-                  {/* name input */}
-                  <label className="label font-bold">Your Name</label>
-                  <input
-                    required
-                    name="name"
-                    type="text"
-                    className="input  mb-2 w-full lg:w-96"
-                    value={user?.displayName}
-                     readOnly
-                  />
-
-                  {/* email input */}
-                  <label className="label font-bold">Your Email</label>
-                  <input
-                    required
-                    name="email"
-                    type="text"
-                    className="input  mb-2 w-full lg:w-96"
-                    value={user?.email}
-                     readOnly
-                  />
-
-                    {/* budget input */}
-                  <label className=" font-bold label">Budget</label>
-                  <input
-                    type="number"
-                    required
-                    name="budget"
-                    className="input mb-2 w-full lg:w-96"
-                    placeholder="Enter your task budget"
-                  />
-
-                  {/* Photo input */}
-                  <label className=" font-bold label">Task Photo URL</label>
-                  <input
-                    required
-                    type="text"
-                    name="photo"
-                    className="input mb-2 w-full lg:w-96"
-                    placeholder="Enter the URL of your task photo"
-                  />
-                </fieldset>
-              </div>
-
-              <button
-                type="submit"
-                className="btn col-span-full flex w-full lg:w-[845px] relative rounded px-5 py-2.5 overflow-hidden group bg-[#49785b]  hover:bg-gradient-to-r hover:from-[#49785b] hover:to-[#49785b] text-white hover:ring-2 hover:ring-offset-2 hover:ring-[#49785b] transition-all ease-out duration-300  "
-              >
-                <span className="absolute right-0 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-50 rotate-12 group-hover:-translate-x-40 ease"></span>
-                <span className="relative">Add Task</span>
-              </button>
-            </form>
-          </div>
+              <span className="absolute right-0 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-50 rotate-12 group-hover:-translate-x-40 ease"></span>
+              <span className="relative">Post</span>
+            </button>
+          </form>
         </div>
       </div>
     </div>
